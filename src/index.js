76756e061eae377acd98f4a5c2c74029d09a4e60
leosoaivan <leo.soaivan@gmail.com>
2019-04-16 22:12:18 -0400

    Add image loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './stylesheets/main.css';
+import ShrimpImage from './images/shrimp.jpeg';
+
+let backgroundImage = new Image('100%', '100%');
+backgroundImage.src = ShrimpImage;
+
+document.body.appendChild(backgroundImage);
