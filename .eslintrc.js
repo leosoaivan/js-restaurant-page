5c34f77e3ec96a6aff60d20434e6ed0ac7701df7
leosoaivan <leo.soaivan@gmail.com>
2019-04-16 21:58:17 -0400

    Fix ESLINT config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,18 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true
-	},
-	'extends': 'eslint:recommended',
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
-	},
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
-	},
-	'rules': {
-		'indent': [
-			'warn',
-			2,
-			2,
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
