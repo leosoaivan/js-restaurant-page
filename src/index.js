c809469f82bacc9c4b08c87f7b6500a6eb8782de
LPSV <leo.soaivan@gmail.com>
2019-04-21 22:31:29 -0400

    Add addressbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import Shrimp from './images/shrimp.jpeg';
+import './stylesheets/main.css';
+import Shrooms from './images/kingoystershrooms.jpeg';
 
 const body = document.getElementById('content');
 
 /**
  * Background
  */
-body.style.backgroundImage = `url(${Shrimp})`;
+body.style.backgroundImage = `url(${Shrooms})`;
 
 /**
  * Address bar
  */
+const addressDom = document.createElement('div');
+const addressText = document.createTextNode('835 North Royal Street, Alexandria, VA 22314');
+
+addressDom.className = 'address';
+addressDom.appendChild(address);
+body.appendChild(addressDom);
